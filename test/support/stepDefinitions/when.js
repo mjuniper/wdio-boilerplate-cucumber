9c1f77fb3ca3f4d729bb453813987be5c68194d2
Jeremy Schneider <jschneider@esri.com>
2017-04-20 08:19:59 -0400

    add comment about origin of obscure uuid generator

--- test/support/stepDefinitions/when.js
+++ test/support/stepDefinitions/when.js
@@ -5,6 +5,8 @@ const examplePage = require('../pages/example.page');
 const opendataSigninPage = require('../pages/opendata-signin.page');
 const opendataPage = require('../pages/opendata.page');
 const opendataAdminPage = require('../pages/opendata-admin.page');
+
+// uuid generator from https://gist.github.com/jed/982883
 b = a=>a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b);
 
 module.exports = function(){
